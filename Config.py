81d1a30be16a40be212a87c297d7318e36414cb1
Naimur001287 <vermanjere61@gmail.com>
2025-07-15 09:55:38 +0600

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+# config.py
+
+API_TOKEN = "7576472064:AAG557fuTOucK71bV7Esbv-77CrRSGit1hw"
+ADMIN_ID = 5781612136  # your Telegram user ID
+
+BASE_PRICE = 1.00  # base price per Gmail account
+CUSTOM_PASS_EXTRA = 0.1  # extra if user gives password
+BACKUP_CODE_EXTRA = 0.05
+TWO_FA_EXTRA = 0.10
+
+RECOVERY_EMAIL = "yourcustomrecovery@gmail.com"
